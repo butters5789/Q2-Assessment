85348e2245780e812b15298ffd65e251c349ba29
Brad Butterfield <butters5789@gmail.com>
2016-04-07 09:45:26 -0600

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: 'postgresql://localhost/knex-people',
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
