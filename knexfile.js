60844194aa4ed9b57e8fa39de43f0d49e9cd61af
Brad Butterfield <butters5789@gmail.com>
2016-04-08 09:48:24 -0600

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,15 +4,16 @@ module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: 'postgresql://localhost/galvanize-reads'
-  },
-  seeds: {
-    directory: './seeds/'
+    connection: process.env.DATABASE_URL,
+    pool : {
+      min: 2,
+      max: 10
+    }
   },
 
   production: {
     client: 'postgresql',
-    cconnection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     pool : {
       min: 2,
       max: 10
